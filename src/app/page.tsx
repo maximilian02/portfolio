e2cfcc9f473dd0383c42cfe400d65e17be89584c
Max Zelarayán <max.z_@hotmail.com>
2023-05-10 10:16:14 -0300

    change and fix image on mobile:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ const HomePage = () => {
   return (
     <>
       <div className="cursor-text flex flex-row justify-around mt-8">
-        <Image
-          src="/me.jpeg"
-          className="rounded-full border border-white transition-all object-scale-down"
-          width={200}
-          height={100}
+        <img
+          src="/me.png"
+          className="rounded-full border border-white w-1/3 md:w-1/6 object-scale-down"
           alt="Max Zelarayán Profile Picture"
         />
       </div>
